12751b39c6156e175fbad3c9faf502629e93c4d6
cesarisim0 <cedgo1997@gmail.com>
2024-01-31 01:54:44 -0400

    FIX: Make navbar shorter

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,16 +1,15 @@
-import React from "react";
 import AccountCircle from "@mui/icons-material/AccountCircle";
-import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import Toolbar from "@mui/material/Toolbar";
 import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import { useRouter } from "next/navigation";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import React from "react";
 import logo from "./../assets/logo.png";
 import { Background } from "./../components/Background";
 import { Footer } from "./../components/Footer";
@@ -22,7 +21,7 @@ const PROFILE = "/Profile";
 const ALBUM = "/Album";
 const STORE = "/Store";
 
-export default function Layout() {
+export default function Layout({ children }) {
     const [anchorElNav, setAnchorElNav] = React.useState(null);
     const [anchorElUser, setAnchorElUser] = React.useState(null);
     const router = useRouter();
@@ -61,121 +60,111 @@ export default function Layout() {
 
     return (
         <>
-            <AppBar
-                position="sticky"
-                style={{
-                    background: "rgb(97,31,63)",
-                    background:
-                        "linear-gradient(500deg, rgba(97,31,63,1) 10%, rgb(83,28,60) 30%, rgb(99,52,79) 70%, rgba(88,49,78,255) 98%)",
-                }}
-                sx={{
-                    borderBottomLeftRadius: "10px",
-                    borderBottomRightRadius: "10px",
-                    borderBottom: 1,
-                    borderBottomColor: "white",
-                    height: 75,
-                    width: { xs: "100%", sm: "100%" },
-                }}
-            >
-                <Toolbar disableGutters>
-                    <Box
-                        sx={{
-                            flexGrow: 0,
-                            marginLeft: 2,
-                            marginRight: 0,
-                            width: "100px",
-                            height: "90px",
-                        }}
-                    >
-                        <Button onClick={handleMainPage}>
-                            <Image
-                                src={logo}
-                                sizes="100vw"
-                                style={{ width: "100%", height: "100%" }}
-                                alt="logo"
-                            />
-                        </Button>
-                    </Box>
+            <Toolbar disableGutters sx={{
+                backgroundColor: "rgba(255, 255, 255, 0.1);",
+                borderRadius: "0 0 15px 15px",
+                borderBottom: "1px solid #FFF",
+                width: "100vw"
+            }}>
+                <Box
+                    sx={{
+                        flexGrow: 0,
+                        marginLeft: 2,
+                        marginRight: 0,
+                        width: "100px",
+                        height: "90px",
+                    }}
+                >
+                    <Button onClick={handleMainPage}>
+                        <Image
+                            src={logo}
+                            sizes="100vw"
+                            style={{ width: "100%", height: "100%" }}
+                            alt="logo"
+                        />
+                    </Button>
+                </Box>
 
-                    <Box
-                        sx={{
-                            flexGrow: 1,
-                            display: { xs: "none", md: "flex" },
-                        }}
-                    >
-                        {pages.map((page) => (
-                            <Button
-                                key={page}
-                                onClick={() => handleClick(page)}
-                                sx={{
-                                    my: 2,
-                                    mx: 1,
-                                    color: "white",
-                                    display: "block",
-                                    textTransform: "none",
-                                    fontWeight: "bold",
-                                    fontSize: "15px",
-                                }}
-                            >
-                                {page}
-                            </Button>
-                        ))}
-                    </Box>
-
-                    <Box sx={{ flexGrow: 0, marginRight: 5 }}>
-                        <Tooltip title="Menú">
-                            <IconButton
-                                size="large"
-                                aria-label="account of current user"
-                                aria-controls="menu-appbar"
-                                aria-haspopup="true"
-                                color="inherit"
-                                onClick={handleOpenUserMenu}
-                                sx={{ p: 0 }}
-                            >
-                                <AccountCircle sx={{ fontSize: 60 }} />
-                            </IconButton>
-                        </Tooltip>
-                        <Menu
+                <Box
+                    sx={{
+                        flexGrow: 1,
+                        display: { xs: "none", md: "flex" },
+                    }}
+                >
+                    {pages.map((page) => (
+                        <Button
+                            key={page}
+                            onClick={() => handleClick(page)}
                             sx={{
-                                mt: "45px",
-                                marginLeft: 4,
-                                marginTop: 7,
-                                "& .MuiMenu-paper": {
-                                    backgroundColor: "#531c3c",
-                                },
+                                my: 2,
+                                mx: 1,
+                                color: "white",
+                                display: "block",
+                                textTransform: "none",
+                                fontWeight: "bold",
+                                fontSize: "15px",
                             }}
-                            id="menu-appbar"
-                            anchorEl={anchorElUser}
-                            anchorOrigin={{
-                                vertical: "top",
-                                horizontal: "right",
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: "top",
-                                horizontal: "right",
-                            }}
-                            open={Boolean(anchorElUser)}
-                            onClose={handleCloseUserMenu}
                         >
-                            {settings.map((setting) => (
-                                <MenuItem
-                                    key={setting}
-                                    onClick={() => handleClickMenu(setting)}
+                            {page}
+                        </Button>
+                    ))}
+                </Box>
+
+                <Box sx={{ flexGrow: 0, marginRight: 5 }}>
+                    <Tooltip title="Menú">
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenUserMenu}
+                            sx={{ p: 0, color: "#FFF" }}
+                        >
+                            <AccountCircle sx={{ fontSize: 60 }} />
+                        </IconButton>
+                    </Tooltip>
+                    <Menu
+                        sx={{
+                            mt: "45px",
+                            marginLeft: 4,
+                            marginTop: 7,
+                            "& .MuiMenu-paper": {
+                                backgroundColor: "#531c3c",
+                            },
+                        }}
+                        id="menu-appbar"
+                        anchorEl={anchorElUser}
+                        anchorOrigin={{
+                            vertical: "top",
+                            horizontal: "right",
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                            vertical: "top",
+                            horizontal: "right",
+                        }}
+                        open={Boolean(anchorElUser)}
+                        onClose={handleCloseUserMenu}
+                    >
+                        {settings.map((setting) => (
+                            <MenuItem
+                                key={setting}
+                                onClick={() => handleClickMenu(setting)}
+                            >
+                                <Typography
+                                    textAlign="center"
+                                    color="common.white"
                                 >
-                                    <Typography
-                                        textAlign="center"
-                                        color="common.white"
-                                    >
-                                        {setting}
-                                    </Typography>
-                                </MenuItem>
-                            ))}
-                        </Menu>
-                    </Box>
-                </Toolbar>
-            </AppBar>
+                                    {setting}
+                                </Typography>
+                            </MenuItem>
+                        ))}
+                    </Menu>
+                </Box>
+            </Toolbar>
+
+            {children}
+
             <Footer />
             <Background />
         </>
--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -6,16 +6,32 @@ import Layout from "../layout/Layout";
 export default function Home() {
     return (
         <>
-            <Layout />
-            <SearchBar />
-            <div> Mi álbum </div>
-            <Card
-                number={10}
-                name={"Lionel Messi"}
-                height={"1.70m"}
-                weight={"72kg"}
-                position={"DC"}
-            />
+            <Layout>
+                <h1>VIVA LA EMOCIÓN DEL DEPORTE REY</h1>
+                <div className="card-pack">
+                    <Card
+                        number={10}
+                        name={"Lionel Messi"}
+                        height={"1.70m"}
+                        weight={"72kg"}
+                        position={"DC"}
+                    />
+                    <Card
+                        number={10}
+                        name={"Lionel Messi"}
+                        height={"1.70m"}
+                        weight={"72kg"}
+                        position={"DC"}
+                    />
+                    <Card
+                        number={10}
+                        name={"Lionel Messi"}
+                        height={"1.70m"}
+                        weight={"72kg"}
+                        position={"DC"}
+                    />
+                </div>
+            </Layout>
         </>
     );
 }
