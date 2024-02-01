e2187fb2578172ffbeb31080f0b59b5c467f890b
adelina <adelinafigueiraf@gmail.com>
2024-02-01 03:36:01 -0400

    footer buttons and searchbar modifications

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,19 +1,20 @@
-import { faHandPointer } from "@fortawesome/fontawesome-free-solid";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Box, TextField } from "@mui/material";
 import React from "react";
 
 export const SearchBar = () => {
     return (
-        <Box component="form" noValidate autoComplete="off">
+        <Box component="form" noValidate autoComplete="off" sx={{width:'100%'}}>
             <TextField
                 label="Buscar..."
                 styles={{ backgroundColor: "white" }}
-                sx={{ borderRadius: 3 }}
+                sx={{ borderRadius: 10, width : '100%'}}
                 variant="filled"
                 InputProps={{
-                    endAdornment: <FontAwesomeIcon icon={faHandPointer} />,
-                    style: { color: "white" },
+                    endAdornment: <FontAwesomeIcon icon={faMagnifyingGlass} />,
+                    style : {backgroundColor : 'white', color: 'red', borderRadius:10},
+                    disableUnderline: true
                 }}
             />
         </Box>
