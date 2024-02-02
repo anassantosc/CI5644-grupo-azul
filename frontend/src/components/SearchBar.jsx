a11c3da6e3ac98112e813c200a171d3c25cd6683
adelina <adelinafigueiraf@gmail.com>
2024-02-02 00:27:18 -0400

    search bar completed

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,19 +1,34 @@
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Box, TextField } from "@mui/material";
+import { IconButton } from '@mui/material';
+
 import React from "react";
 
 export const SearchBar = () => {
+    const searchQuery = (query) => {
+        console.log("query")
+    }
     return (
-        <Box component="form" noValidate autoComplete="off" sx={{width:'100%'}}>
+        <Box component="form" noValidate autoComplete="off" sx={{width:'100%', height : '100px'}}>
             <TextField
                 label="Buscar..."
-                styles={{ backgroundColor: "white" }}
-                sx={{ borderRadius: 10, width : '100%'}}
+                onInput={(e) => {
+                    searchQuery(e.target.value);
+                  }}
+                sx={{   borderRadius: 10, width : '100%',
+                        "& .MuiFormLabel-root": {
+                            color: "#370001"
+                        },
+                        "& .MuiFormLabel-root.Mui-focused": {
+                            color: "#370001"
+                        }
+                    }}
                 variant="filled"
                 InputProps={{
-                    endAdornment: <FontAwesomeIcon icon={faMagnifyingGlass} />,
-                    style : {backgroundColor : 'white', color: 'red', borderRadius:10},
+                    color : 'red',
+                    endAdornment: <IconButton type="submit" aria-label="search" style={{ color:"#370001" }}> <FontAwesomeIcon icon={faMagnifyingGlass} /> </IconButton>,
+                    style : {backgroundColor : 'white', color: "#370001", borderRadius:10},
                     disableUnderline: true
                 }}
             />
