afb665f7681701725c9440ca5e24de0358ab0f97
Simon <18-10717@usb.ve>
2024-02-08 09:15:45 -0400

    MODIFIED: loginDTO & registerDTO

---
+++ backend/src/main/kotlin/com/ci5644/trade/dto/auth/RegisterDto.kt
@@ -0,0 +1,33 @@
+package com.ci5644.trade.dto.auth
+
+import com.fasterxml.jackson.annotation.JsonFormat
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Pattern
+import jakarta.validation.constraints.Size
+import java.time.LocalDate
+
+data class RegisterDTO(
+    @NotNull
+    @NotEmpty
+    @Size(max = 16)
+    @Pattern(regexp = "^[A-Za-z0-9]+$")
+    val username: String,
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 3)
+    val password: String,
+
+    @NotNull
+    @NotEmpty
+    @Size(max = 64)
+    val name: String,
+
+    @NotNull
+    @NotEmpty
+    @Size(max = 32)
+    val email: String,
+
+    val gender: String?
+)
