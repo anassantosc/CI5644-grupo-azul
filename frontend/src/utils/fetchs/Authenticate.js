fde8efd6a460469181177b479347ce2914952a6b
cesarisim0 <cedgo1997@gmail.com>
2024-02-16 19:03:26 -0400

    ADD: Authenticate utils to make fetch to Login, Register and Logout

---
+++ frontend/src/utils/fetchs/Authenticate.js
@@ -0,0 +1,33 @@
+import secureFetch from "./SecureFetch";
+
+export const Authenticate = async (userData, authType) => {
+    const url = `${process.env.NEXT_PUBLIC_API_URL}/url/auth/${authType}`;
+    const method = "POST";
+    const data = JSON.stringify(userData);
+    const headers = {};
+
+    const response = await secureFetch(url, method, data, headers);
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    if (authType === "login") {
+        return await response.json();
+    }
+};
+
+export const Logout = async () => {
+    const url = `${process.env.NEXT_PUBLIC_API_URL}/url/auth/logout`;
+    const method = "GET";
+    const data = "";
+    const headers = {};
+
+    const response = await secureFetch(url, method, data, headers);
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    return await response.json();
+};
