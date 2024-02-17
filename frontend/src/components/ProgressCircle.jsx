1363e61d47ef89c106ee11f3b1aff5c4163a0aee
cesarisim0 <cedgo1997@gmail.com>
2024-02-17 17:58:53 -0400

    FIX: Profile proptype errors fixed

--- frontend/src/components/ProgressCircle.jsx
+++ frontend/src/components/ProgressCircle.jsx
@@ -1,39 +1,65 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import { styled } from '@mui/system';
-import CircularProgress, { circularProgressClasses } from '@mui/material/CircularProgress';
+import * as React from "react";
+import PropTypes from "prop-types";
+import { styled } from "@mui/system";
+import CircularProgress, {
+    circularProgressClasses,
+} from "@mui/material/CircularProgress";
 
 const StyledCircularProgress = styled(CircularProgress)(({ theme }) => ({
-  position: 'absolute',
-  [`&.${circularProgressClasses.colorPrimary}`]: {
-    color: 'white', // Este es el circulo de progreso
-  },
-  [`&.${circularProgressClasses.colorSecondary}`]: {
-    color: '#731530', // Este es el circulo de fondo
-  }
+    position: "absolute",
+    [`&.${circularProgressClasses.colorPrimary}`]: {
+        color: "white", // Este es el circulo de progreso
+    },
+    [`&.${circularProgressClasses.colorSecondary}`]: {
+        color: "#731530", // Este es el circulo de fondo
+    },
 }));
 
 const ProgressBar = ({ value }) => {
-  return (
-    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '200px'}}>
-      <StyledCircularProgress variant="determinate" size={200} thickness={6} value={100} color="secondary" />
-      <StyledCircularProgress variant="determinate" size={200} thickness={6} value={value} color="primary" />
-      <div style={{
-        position: 'absolute',
-        display: 'flex', 
-        justifyContent: 'center', 
-        alignItems: 'center', 
-        width: '200px', 
-        height: '200px', 
-        color: 'white', 
-        fontSize: '35px', 
-        fontWeight: 'semi-bold'}}>{value}%</div>
-    </div>
-  );
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "200px",
+            }}
+        >
+            <StyledCircularProgress
+                variant="determinate"
+                size={200}
+                thickness={6}
+                value={100}
+                color="secondary"
+            />
+            <StyledCircularProgress
+                variant="determinate"
+                size={200}
+                thickness={6}
+                value={Number(value)}
+                color="primary"
+            />
+            <div
+                style={{
+                    position: "absolute",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    width: "200px",
+                    height: "200px",
+                    color: "white",
+                    fontSize: "35px",
+                    fontWeight: "semi-bold",
+                }}
+            >
+                {value}%
+            </div>
+        </div>
+    );
 };
 
 ProgressBar.propTypes = {
-  value: PropTypes.number.isRequired,
+    value: PropTypes.string.isRequired,
 };
 
 export default ProgressBar;
