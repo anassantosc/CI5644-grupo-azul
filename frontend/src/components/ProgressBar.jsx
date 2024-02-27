1431cd146ec482cac6ed9e8124537b2f5fcfb3ad
Simon Puyosa <18-10717@usb.ve>
2024-02-27 15:46:50 -0400

    MODIFIED: useState of cardStatus, merged into one

--- frontend/src/components/ProgressBar.jsx
+++ frontend/src/components/ProgressBar.jsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import PropTypes from "prop-types";
-import { styled } from "@mui/system";
 import LinearProgress from "@mui/material/LinearProgress";
 import styles from "../../styles/ProgressBar.module.css";
+
 const ProgressBar = ({ value }) => {
     return <LinearProgress className={styles.linearProgress} variant="determinate" value={value} />;
 };
