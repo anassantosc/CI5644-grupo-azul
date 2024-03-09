09e63add01eeaae684b079baf2a923d2c95d35a5
Jeamhowards Montiel <19-10234@usb.ve>
2024-03-09 18:03:16 -0400

    Adds showAlert parameter to AcceptOffer and DenyOffer functions

--- frontend/src/utils/fetchs/AcceptOffer.js
+++ frontend/src/utils/fetchs/AcceptOffer.js
@@ -1,7 +1,7 @@
 import secureFetch from "./SecureFetch";
-import { HTTPMethods, routes, alertMessages } from "../constants";
+import { HTTPMethods, routes, alertMessages, alertTypes, statusCodes} from "../constants";
 
-export const AcceptOffer = async (offerId) => {
+export const AcceptOffer = async (offerId, showAlert) => {
     const url = `${process.env.NEXT_PUBLIC_API_URL}${routes.api}${routes.offer}${routes.accept}`;
     const method = HTTPMethods.POST;
     const data = JSON.stringify({ offerId });
@@ -10,8 +10,12 @@ export const AcceptOffer = async (offerId) => {
     try {
         const response = await secureFetch(url, method, data, headers);
 
-        if (!response.ok) {
-            throw new Error(`${alertMessages.http_error} ${response.status}`);
+        if(response.ok) {
+            showAlert(alertMessages.accept_trade, alertTypes.success)
+        } else if (response.status === statusCodes.bad_request) {
+            showAlert(response.message, alertTypes.error)
+        } else {
+            showAlert(alertMessages.unknown_error, alertTypes.error);
         }
         return response;
     } catch (error) {
--- frontend/src/utils/fetchs/DenyOffer.js
+++ frontend/src/utils/fetchs/DenyOffer.js
@@ -1,7 +1,7 @@
 import secureFetch from "./SecureFetch";
-import { HTTPMethods, routes, alertMessages } from "../constants";
+import { HTTPMethods, routes, alertMessages, alertTypes, statusCodes} from "../constants";
 
-export const DenyOffer = async (offerId) => {
+export const DenyOffer = async (offerId, showAlert) => {
     const url = `${process.env.NEXT_PUBLIC_API_URL}${routes.api}${routes.offer}${routes.deny}`;
     const method = HTTPMethods.POST;
     const data = JSON.stringify({ offerId });
@@ -10,8 +10,12 @@ export const DenyOffer = async (offerId) => {
     try {
         const response = await secureFetch(url, method, data, headers);
 
-        if (!response.ok) {
-            throw new Error(`${alertMessages.http_error} ${response.status}`);
+        if(response.ok) {
+            showAlert(alertMessages.deny_trade, alertTypes.warning)
+        } else if (response.status === statusCodes.bad_request) {
+            showAlert(response.message, alertTypes.error)
+        } else {
+            showAlert(alertMessages.unknown_error, alertTypes.error);
         }
         return response;
     } catch (error) {
