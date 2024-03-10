8bdda2828b44028e2ba60a0ebc16dd3ec12ce9a5
cesarisim0 <cedgo1997@gmail.com>
2024-03-10 00:47:37 -0400

    FIX: Offer Modal styles fixed

--- frontend/src/components/StyledTableRow.jsx
+++ frontend/src/components/StyledTableRow.jsx
@@ -1,17 +1,10 @@
-
 import {
     styled,
     TableRow
 } from "@mui/material";
 
-export const StyledTableRow = styled(TableRow)(({ theme }) => ({
+export const StyledTableRow = styled(TableRow)(({theme}) => ({
     '&:nth-of-type(odd)': {
         backgroundColor: theme.palette.action.hover,
-    },
-    '& > *': {
-        borderBottom: 'unset',
-        height: '50px',
-        padding: "0px",
-        textAlign: 'center'
     },
 }));
--- frontend/src/components/TableSelector.jsx
+++ frontend/src/components/TableSelector.jsx
@@ -10,17 +10,17 @@ import {
     Typography,
     Box
 } from "@mui/material";
-import { useEffect, useState, useMemo } from "react";
-import { StyledTableRow } from "./StyledTableRow";
+import {useEffect, useState, useMemo} from "react";
+import {StyledTableRow} from "./StyledTableRow";
 import styles from './../../styles/TableSelector.module.css';
-import { GetWishlist } from "../utils/fetchs/GetWishlist";
-import { GetDuplicatedCards } from "../utils/fetchs/GetDuplicatedCards";
+import {GetWishlist} from "../utils/fetchs/GetWishlist";
+import {GetDuplicatedCards} from "../utils/fetchs/GetDuplicatedCards";
 import CircularProgress from '@mui/material/CircularProgress';
 
 
 const pageSize = 20;
 
-export const TableSelector = ({ onSelect, onClick, receive, offer = null }) => {
+export const TableSelector = ({onSelect, onClick, receive, offer = null}) => {
     const [page, setPage] = useState(0);
     const [rows, setRows] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
@@ -74,20 +74,20 @@ export const TableSelector = ({ onSelect, onClick, receive, offer = null }) => {
     };
 
     return (
-        <TableContainer component={Paper} className={styles.tableContainer} >
-            <Typography className={styles.title} variant="h6" gutterBottom >
+        <TableContainer component={Paper} className={styles.tableContainer}>
+            <Typography className={styles.title} variant="h6" gutterBottom>
                 {receive ? 'Elija la carta a recibir' : 'Elija la carta a ofrecer'}
             </Typography>
 
             {isLoading && (
-                <Box className="loading-container">
-                    <CircularProgress />
-                    <Typography variant="body1" className="loading-text">Cargando...</Typography>
+                <Box className={styles.loadingContainer}>
+                    <CircularProgress/>
+                    <Typography variant="body1" className={styles.loadingText}>Cargando...</Typography>
                 </Box>
             )}
 
             {!isLoading && rows.length === 0 && page === 0 && (
-                <Typography variant="h4" className="status-message">
+                <Typography variant="h4" className={styles.statusMessage}>
                     {receive ?
                         (offer ? "No hay ninguna carta que le falte y que el usuario a contraofertar tenga duplicada" : "FELICIDADES ya completaste el álbum, no te falta ninguna carta")
                         :
@@ -95,14 +95,16 @@ export const TableSelector = ({ onSelect, onClick, receive, offer = null }) => {
                     }
                 </Typography>
             )}
-            
+
             {!isLoading && (rows.length > 0 || page > 0) && (
                 <Table>
                     <TableBody>
                         {rows.map((row, rowIndex) => (
                             <StyledTableRow key={rowIndex}>
                                 {row.map((card, index) => (
-                                    <TableCell key={index}>
+                                    <TableCell key={index} sx={{
+                                        border: 'none'
+                                    }}>
                                         <Button onClick={() => handleSelect(card)} className={styles.cardButton}>
                                             Carta {card}
                                         </Button>
@@ -119,7 +121,7 @@ export const TableSelector = ({ onSelect, onClick, receive, offer = null }) => {
                                 rowsPerPage={pageSize}
                                 rowsPerPageOptions={[20]}
                                 onPageChange={handleChangePage}
-                                labelDisplayedRows={({ from, to, count }) => `${from}-${to}`}
+                                labelDisplayedRows={({from, to, count}) => `${from}-${to}`}
                                 nextIconButtonProps={{
                                     disabled: rows.length < 5 || rows[4].length < 4 || page * pageSize + pageSize >= 640,
                                 }}
