d3f4dd11da179172df981947f1fa0a19b14ef40e
Jeamhowards Montiel <19-10234@usb.ve>
2024-04-03 02:05:56 -0400

    Adds SecureRandomUtil for generating random integers

---
+++ backend/src/main/kotlin/com/ci5644/trade/utils/SecureRandomUtil.kt
@@ -0,0 +1,16 @@
+import java.security.SecureRandom
+
+object SecureRandomUtil {
+    private val secureRandom = SecureRandom()
+
+    /**
+     * Generate a random integer within the specified range.
+     *
+     * @param min The minimum value of the range (inclusive)
+     * @param max The maximum value of the range (inclusive)
+     * @return A random integer within the specified range
+     */
+    fun getRandomInt(min: Int, max: Int): Int {
+        return secureRandom.nextInt(max - min + 1) + min
+    }
+}
