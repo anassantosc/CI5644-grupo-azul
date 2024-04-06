002fd29acb23f284ab5d120289f21e7e4cc88cc5
cesarisim0 <cedgo1997@gmail.com>
2024-04-06 10:25:55 -0400

    ADD: New animated bought card effect added with dummy data

--- frontend/src/pages/store.jsx
+++ frontend/src/pages/store.jsx
@@ -1,62 +1,68 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import Layout from "../layout/Layout";
-import { Box, Grid, Typography, TextField, Button } from "@mui/material";
+import {Box, Grid, Typography, TextField, Button, Link} from "@mui/material";
 import styles from "../../styles/Store.module.css";
-import { useAlert } from "../context/AlertContext";
-
+import {useAlert} from "../context/AlertContext";
+import {FlipCard} from "../components/FlipCard";
+import {boughtCards} from "./storeTemp";
 
 function Store() {
     const [packages, setPackages] = useState(1);
     const [showPayment, setShowPayment] = useState(false);
     const [payment, setPayment] = useState(0);
     const [paymentSuccess, setPaymentSuccess] = useState(false);
+    const [revealed, setRevealed] = useState(false);
+    const [packOpenProgress, setPackOpenProgress] = useState(0);
     const showAlert = useAlert();
+
 
     const handleBuyBox = (quantity) => {
         setPackages(quantity);
         showAlert('Seleccionaste una caja', 'info');
         setShowPayment(true);
     }
-    
+
     const handleBuyPackage = (quantity) => {
         setPackages(quantity);
         showAlert('Seleccionaste nuevos paquetes de barajitas', 'info');
         setShowPayment(true);
     }
-    
-    const handlePayment = (bool) => {
-        if (bool) {
-            showAlert('Compra exitosa', 'success');
-            setPaymentSuccess(true);
-        } else {
-            setShowPayment(false);
-            showAlert('Compra cancelada', 'error');
+
+    const handlePayment = (paymentDone) => {
+        paymentDone ? showAlert('Compra exitosa', 'success') : showAlert('Compra cancelada', 'error');
+        setPaymentSuccess(paymentDone);
+    }
+
+    const handleNextPack = () => {
+        setRevealed(false);
+        if (packOpenProgress < boughtCards.length - 1) {
+            setPackOpenProgress(packOpenProgress + 1);
         }
     }
-    
-    if (showPayment) {
+
+    if (showPayment && !paymentSuccess) {
         return (
             <Layout>
                 <Box className={styles.storeBox}>
                     <Grid container spacing={2} alignItems="center">
-                        <Grid item xs={6} >
+                        <Grid item xs={6}>
                             <Typography variant="h6" className={styles.subtitle}>
                                 Monto a pagar: ${payment}
                             </Typography>
                             <div className={styles.buttonContainer}>
-                                <Button 
+                                <Button
                                     variant="contained"
                                     color="primary"
                                     className={styles.payButton}
                                     onClick={() => handlePayment(true)}
                                 >
                                     Pagar
-                                </Button>   
+                                </Button>
                                 <Button
                                     variant="contained"
                                     size="medium"
-                                    onClick={() => handlePayment(false)} 
-                                    className={styles.cancelButton}    
+                                    onClick={() => handlePayment(false)}
+                                    className={styles.cancelButton}
                                 >
                                     Cancelar
                                 </Button>
@@ -71,12 +77,56 @@ function Store() {
     if (paymentSuccess) {
         return (
             <Layout>
-                <Box className={styles.storeBox}>
-                    <Grid container spacing={2} alignItems="center">
-                        <Grid item xs={6} >
-                            <a href="">Visualizar barajitas nuevas</a>
-                        </Grid>
-                    </Grid>
+                <Box className={styles.boughtCardsBox}>
+                    <Typography variant="h3" className={styles.boughtTitle}>
+                        ¡Descubre tus nuevas barajitas!
+                    </Typography>
+                    <Box className={styles.cardsContainer}>
+
+                        {
+                            boughtCards[packOpenProgress].map(
+                                card => (
+                                    <FlipCard
+                                        key={card.id}
+                                        name={card ? card.playerName : null}
+                                        number={card.id}
+                                        position={card ? card.position : null}
+                                        height={card ? card.height + "m" : "0.0m"}
+                                        weight={card ? card.weight.toFixed(1) + "Kg" : "0.0Kg"}
+                                        flipped={revealed}
+                                    />
+                                )
+                            )
+                        }
+                    </Box>
+
+                    <Typography variant="h6" className={styles.boughtLength}>
+                        {
+                            `${packOpenProgress + 1}/${boughtCards.length} paquetes restantes`
+                        }
+                    </Typography>
+                    <Box className={styles.packButtonGroup}>
+                        <Button
+                            variant="outlined"
+                            size="medium"
+                            onClick={() => setRevealed(true)}
+                            className={styles.nextPackButton}
+                        >
+                            Revelar barajitas
+                        </Button>
+                        <Button
+                            variant="contained"
+                            size="medium"
+                            onClick={handleNextPack}
+                            className={styles.nextPackButton}
+                            disabled={packOpenProgress === boughtCards.length - 1}
+                        >
+                            Siguiente
+                        </Button>
+                    </Box>
+                    <Link href="/album" className={styles.packLink}>
+                        O bien, comprueba tu álbum
+                    </Link>
                 </Box>
             </Layout>
         );
@@ -86,7 +136,7 @@ function Store() {
         <Layout>
             <Box className={styles.storeBox}>
                 <Grid container spacing={2} alignItems="center">
-                    <Grid 
+                    <Grid
                         item
                         xs={8}
                         container
@@ -97,25 +147,25 @@ function Store() {
                             Tienda de Barajitas
                         </Typography>
                     </Grid>
-                    <Grid 
-                        item 
-                        xs={6} 
-                        justifyContent="center" 
-                        alignItems="center" 
+                    <Grid
+                        item
+                        xs={6}
+                        justifyContent="center"
+                        alignItems="center"
                     >
                         <Typography variant="h6" className={styles.subtitle}>
                             Comprar una caja
                         </Typography>
-                        <Button 
+                        <Button
                             variant="contained"
                             color="primary"
                             className={styles.saleButton}
-                            onClick={() => handleBuyBox('box',100)}
+                            onClick={() => handleBuyBox('box', 100)}
                         >
                             Comprar
-                        </Button>        
+                        </Button>
                     </Grid>
-                    <Grid 
+                    <Grid
                         item
                         xs={6}
                         container
@@ -134,12 +184,12 @@ function Store() {
                             size="small"
                             color="secondary"
                             className={styles.textPackage}
-                            inputProps={{ min: 1, max: 99 }}
+                            inputProps={{min: 1, max: 99}}
                         />
                         <Typography variant="h6" className={styles.subtitle}>
                             paquetes
                         </Typography>
-                        <Button 
+                        <Button
                             variant="contained"
                             color="primary"
                             className={styles.saleButton}
---
+++ frontend/src/pages/storeTemp.js
@@ -0,0 +1,94 @@
+export const boughtCards = [
+    [{
+        "id": 1,
+        "playerName": "Cristian Strathdee",
+        "country": "Alemania",
+        "shirtNumber": 1,
+        "position": "por",
+        "height": 1.93,
+        "weight": 89.49
+    },
+        {
+            "id": 2,
+            "playerName": "Bordie Vaissiere",
+            "country": "Alemania",
+            "shirtNumber": 2,
+            "position": "por",
+            "height": 1.96,
+            "weight": 75.61
+        },
+        {
+            "id": 3,
+            "playerName": "Kippar Reddyhoff",
+            "country": "Alemania",
+            "shirtNumber": 3,
+            "position": "por",
+            "height": 2.02,
+            "weight": 79.27
+        },
+        {
+            "id": 5,
+            "playerName": "Ivan Buddleigh",
+            "country": "Alemania",
+            "shirtNumber": 5,
+            "position": "def",
+            "height": 1.63,
+            "weight": 85.93
+        },
+        {
+            "id": 6,
+            "playerName": "Niles Whewill",
+            "country": "Alemania",
+            "shirtNumber": 6,
+            "position": "def",
+            "height": 2.07,
+            "weight": 85.02
+        }
+    ],
+    [
+        {
+            "id": 4,
+            "playerName": "Rodolfo Drust",
+            "country": "Alemania",
+            "shirtNumber": 4,
+            "position": "li",
+            "height": 1.73,
+            "weight": 84.2
+        },
+        {
+            "id": 2,
+            "playerName": "Bordie Vaissiere",
+            "country": "Alemania",
+            "shirtNumber": 2,
+            "position": "por",
+            "height": 1.96,
+            "weight": 75.61
+        },
+        {
+            "id": 3,
+            "playerName": "Kippar Reddyhoff",
+            "country": "Alemania",
+            "shirtNumber": 3,
+            "position": "por",
+            "height": 2.02,
+            "weight": 79.27
+        },
+        {
+            "id": 5,
+            "playerName": "Ivan Buddleigh",
+            "country": "Alemania",
+            "shirtNumber": 5,
+            "position": "def",
+            "height": 1.63,
+            "weight": 85.93
+        },
+        {
+            "id": 6,
+            "playerName": "Niles Whewill",
+            "country": "Alemania",
+            "shirtNumber": 6,
+            "position": "def",
+            "height": 2.07,
+            "weight": 85.02
+        }],
+]
